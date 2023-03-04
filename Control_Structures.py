761faea240332a1e73dc6bec9e35545e44bca197
Ceejay.Sakaii <ceejaysakaii@gmail.com>
2023-03-04 18:08:50 +0800

    control structures

---
+++ Control_Structures.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#!/usr/bin/env python3
+
+import sys    #used for sys.exit function
+
+int_num = 7
+
+if int_num < 6:
+    sys.exit("int_num must be greater than 5")
+else:
+    print("int_num detected as > 5 - continuing...")
+    
