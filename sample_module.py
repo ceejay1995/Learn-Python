01d43349a42c1f23c53793424c41db337d56e435
Ceejay.Sakaii <ceejaysakaii@gmail.com>
2023-03-09 11:43:37 +0800

    sample_module created

---
+++ sample_module.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# creating module
+
+# module is just group of functions
+# where main program must import file before functions can be called
+
+# function that add 2 numbers
+# return value as integer
+
+def sum(x,y):
+    val = x + y
+    return val
+
